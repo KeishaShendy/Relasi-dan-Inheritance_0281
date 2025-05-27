30fb65789474ce5d67beb6bbd5a0370376eee92e
keishashendy <shendyarrafli7@gmail.com>
2025-05-27 12:16:50 +0700

    Menambahkan kelas pelajar yang mewarisi kelas orang dengan konstruktor, destruktor, dan metode perkenalan

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -19,3 +19,28 @@ class orang {
             return a + b;
         }
 };
+
+class pelajar : public orang {
+    public:
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+            cout << "Pelajar dibuat" << endl;
+        }
+
+        ~pelajar() {
+            cout << "Pelajar dihpuskan" << endl;
+        }
+
+        string perkenalan() {
+            return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n";
+        }
+};
+
+int main() {
+    pelajar siswa("Aniki Iksano", "SMA 1 Bantul");
+    cout << siswa.perkenalan();
+    cout << "Jumlah = " << siswa.jumlah(10, 20) << endl;
+
+    return 0;
+}
